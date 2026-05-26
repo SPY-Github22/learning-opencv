7bce69f02689dd3c647184aa4aa5a6fbde893d56
SPY-Github22 <sudpy2022@gmail.com>
2026-05-26 12:13:54 +0530

    split and merge proper with proper key closing

--- first.py
+++ first.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import keyboard
 
 #print(cv2.imread("image.jpg", cv2.IMREAD_COLOR))
 #print(cv2.imread("image.jpg", cv2.IMREAD_GRAYSCALE))
@@ -13,10 +12,9 @@ img1 = cv2.imread("image.jpg")
 
 cv2.imshow('Window', img1)
 
-cv2.waitKey(0)
 while True:
-    if keyboard.is_pressed('s'): #to use letters as escape character to exit window
-        cv2.waitKey(1)
+    key = cv2.waitKey(0)
+    if key == ord('s'): #Converts 's' to ASCII character
         break
 
 
@@ -25,9 +23,24 @@ print(cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY))
 b,g,r = cv2.split(img1) #passes tuple of three arrays #colours are split
 
 cv2.imshow('Blue', b)
-cv2.waitKey('b')
+key = cv2.waitKey(0)
+if key == ord('b'):
+    cv2.destroyWindow('Blue')
+
 cv2.imshow('Green', g)
-cv2.waitKey('g')
+key = cv2.waitKey(0)
+if key == ord('g'):
+    cv2.destroyWindow('Green')
+
 cv2.imshow('Red', r)
-cv2.waitKey('r')
+key = cv2.waitKey(0)
+if key == ord('r'):
+    cv2.destroyWindow('Red')
+
+x=cv2.merge([b,r,g])
+cv2.imshow('merged',x)
+cv2.waitKey(0)
+
+print(np.array_equal(img1, x))
+print(np.array_equal(img1, cv2.merge([b,g,r])))
 
